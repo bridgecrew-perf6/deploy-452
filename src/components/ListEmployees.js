250932afbe88838e7227cd083cc90b00efa04c37
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-19 09:54:22 +0300

    Update ListEmployees.js

--- src/components/ListEmployees.js
+++ src/components/ListEmployees.js
@@ -9,7 +9,7 @@ export default class ListEmployees extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      employees: [""],
+      employees: [ ],
     };
   }
   // addEmployee=()=>{
