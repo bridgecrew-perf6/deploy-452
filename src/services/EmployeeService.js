162ba18763c0945273d9f6febb7a000dd2fb605e
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-19 09:14:19 +0300

    Update EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "https://murmuring-lake-94113.herokuapp.com/api/bootcamp";
+const EMPLOYEE_API_BASE_URL = "/api/bootcamp";
 
 class EmployeeService {
   getEmployees() {
